1bebcdd8e714e184e2f860c849124b76df124d33
Quarx2k <agent00791@gmail.com>
2012-07-22 15:17:07 +0700

    Add LegacyUsbDeviceManager::setCurrentFunction method. (from ics, adapted to jb)
    Conflicts:
    	services/java/com/android/server/usb/LegacyUsbDeviceManager.java

--- services/java/com/android/server/usb/LegacyUsbDeviceManager.java
+++ services/java/com/android/server/usb/LegacyUsbDeviceManager.java
@@ -722,6 +722,12 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
         }
     }
 
+    @Override
+    public void setCurrentFunctions(String function, boolean makeDefault) {
+        if (DEBUG) Slog.d(TAG, "setCurrentFunction(" + function + ") default: " + makeDefault);
+        mHandler.sendMessage(MSG_SET_CURRENT_FUNCTION, function, makeDefault);
+    }
+
     public void allowUsbDebugging(boolean alwaysAllow, String publicKey) {
         if (mDebuggingManager != null) {
             mDebuggingManager.allowUsbDebugging(alwaysAllow, publicKey);
