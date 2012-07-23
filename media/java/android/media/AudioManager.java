4f293d335f24d36e800e4da1630f77a59a1197f6
Quarx2k <agent00791@gmail.com>
2012-07-23 17:41:12 +0700

    add STREAM_FM
    Conflicts:
    
    	media/java/android/media/AudioManager.java
    	media/java/android/media/AudioSystem.java

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -224,6 +224,8 @@ public class AudioManager {
     public static final int STREAM_DTMF = AudioSystem.STREAM_DTMF;
     /** @hide The audio stream for text to speech (TTS) */
     public static final int STREAM_TTS = AudioSystem.STREAM_TTS;
+    /** @hide The audio stream for FM Radio */
+    public static final int STREAM_FM = AudioSystem.STREAM_FM;
     /** @hide The audio stream for incall music delivery */
     public static final int STREAM_INCALL_MUSIC = AudioSystem.STREAM_INCALL_MUSIC;
     /** Number of audio streams */
@@ -245,7 +247,9 @@ public class AudioManager {
         7,  // STREAM_SYSTEM_ENFORCED
         11, // STREAM_DTMF
         11, // STREAM_TTS
+        7,  // STREAM_FM
         4   // STREAM_INCALL_MUSIC
+        
     };
 
     /**
