b1b6b5a20e63aa7d75fc507a7425e8eebb4d900d
Christopher Tate <ctate@google.com>
2012-11-16 15:58:08 -0800

    Cache resource ID lookups in aapt
    This speeds up certain workloads considerably, particularly
    those involved in buildling apps via the SDK.  Windows-based
    use should particularly benefit from the change.
    
    Change-Id: I29f4b3a77400b201ee219729cc28a5e359c0c5e8
    
    Conflicts:
    	tools/aapt/ResourceIdCache.cpp
    	tools/aapt/ResourceIdCache.h

--- tools/aapt/ResourceIdCache.h
+++ tools/aapt/ResourceIdCache.h
@@ -1,12 +1,17 @@
 //
 // Copyright 2012 The Android Open Source Project
 //
+<<<<<<< HEAD
 // Cache for resIds - we tend to lookup the same thing repeatedly
 //
+=======
+// Manage a resource ID cache.
+>>>>>>> bd9fa42... Cache resource ID lookups in aapt
 
 #ifndef RESOURCE_ID_CACHE_H
 #define RESOURCE_ID_CACHE_H
 
+<<<<<<< HEAD
 #include "StringPool.h"
 
 class ResourceIdCache {
@@ -23,4 +28,27 @@ public:
                       uint32_t resId);
 };
 
+=======
+namespace android {
+class android::String16;
+
+class ResourceIdCache {
+public:
+    static uint32_t lookup(const android::String16& package,
+            const android::String16& type,
+            const android::String16& name,
+            bool onlyPublic);
+
+    static uint32_t store(const android::String16& package,
+            const android::String16& type,
+            const android::String16& name,
+            bool onlyPublic,
+            uint32_t resId);
+
+    static void dump(void);
+};
+
+}
+
+>>>>>>> bd9fa42... Cache resource ID lookups in aapt
 #endif
