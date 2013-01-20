f29b459d47f7fe76ca8b3406d0b02501ec889efa
Kayant <anthonysanwo@googlemail.com>
2013-01-20 11:28:07 +0000

    Fix derp

--- tools/aapt/ResourceIdCache.h
+++ tools/aapt/ResourceIdCache.h
@@ -1,34 +1,11 @@
 //
 // Copyright 2012 The Android Open Source Project
 //
-<<<<<<< HEAD
-// Cache for resIds - we tend to lookup the same thing repeatedly
-//
-=======
 // Manage a resource ID cache.
->>>>>>> bd9fa42... Cache resource ID lookups in aapt
 
 #ifndef RESOURCE_ID_CACHE_H
 #define RESOURCE_ID_CACHE_H
 
-<<<<<<< HEAD
-#include "StringPool.h"
-
-class ResourceIdCache {
-public:
-    static uint32_t lookup(const String16& package,
-                           const String16& type,
-                           const String16& name,
-                           bool onlyPublic);
-
-    static bool store(const String16& package,
-                      const String16& type,
-                      const String16& name,
-                      bool onlyPublic,
-                      uint32_t resId);
-};
-
-=======
 namespace android {
 class android::String16;
 
@@ -50,5 +27,4 @@ public:
 
 }
 
->>>>>>> bd9fa42... Cache resource ID lookups in aapt
 #endif
